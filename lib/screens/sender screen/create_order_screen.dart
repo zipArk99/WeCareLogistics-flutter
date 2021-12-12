63d7469704f9f36771024363a23af81a56170380
Shaurya Kajiwala <kajiwalashaurya29@gmail.com>
2021-12-13 00:09:38 +0530

    zipArk99/dynamic form creation and basic validation added

--- lib/screens/sender screen/create_order_screen.dart
+++ lib/screens/sender screen/create_order_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class CreateOrderScreen extends StatefulWidget {
   static const String createOrderScreenRoute = "/CreateOrderScreenRoute";
@@ -9,6 +11,95 @@ class CreateOrderScreen extends StatefulWidget {
 }
 
 class CreateOrderScreenState extends State<CreateOrderScreen> {
+  var _form = GlobalKey<FormState>();
+  var _isDateSelected = false;
+  String? _selectedDate;
+
+  var _pickUpFocus = FocusNode();
+  var _reciverNameFocus = FocusNode();
+  var _dropLocationFocus = FocusNode();
+  var _deliveryPinCodeFocus = FocusNode();
+  var _lengthFocus = FocusNode();
+  var _breadthFocus = FocusNode();
+  var _heightFocus = FocusNode();
+  var _weightFocus = FocusNode();
+  var _productTitleFocus = FocusNode();
+  var _productCategoryFocus = FocusNode();
+  var _productPriceFocus = FocusNode();
+  var _quantityFocus = FocusNode();
+
+  Future<void> opendDatePicker() async {
+    var _date = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2023));
+
+    if (_date != null) {
+      setState(() {
+        _selectedDate = DateFormat('d/M/y').format(_date);
+        _isDateSelected = true;
+      });
+    } else {
+      setState(() {
+        _selectedDate = null;
+        _isDateSelected = false;
+      });
+    }
+  }
+
+  Widget createTextFormField(
+      {required BuildContext contx,
+      required String label,
+      TextInputType keyboard = TextInputType.text,
+      required FocusNode focus,
+      required FocusNode requestF,
+      required String? Function(String?) funValidate}) {
+    return TextFormField(
+        autovalidateMode: AutovalidateMode.always,
+        keyboardType: keyboard,
+        textInputAction: TextInputAction.next,
+        focusNode: focus,
+        decoration: InputDecoration(labelText: label),
+        onFieldSubmitted: (_) {
+          Focus.of(contx).requestFocus(requestF);
+        },
+        validator: funValidate);
+  }
+
+  String? checkIsEmpty(String? value) {
+    if (value!.isEmpty) {
+      return "Empty Field!";
+    }
+    return null;
+  }
+
+  void onCreateOrder() {
+    var validate = _form.currentState!.validate();
+
+    if (!validate) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Error in Form"),
+              content: Text("Please fill whole form"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Okay',
+                  ),
+                )
+              ],
+            );
+          });
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext contx) {
     return Scaffold(
@@ -31,6 +122,7 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
         child: SingleChildScrollView(
           child: Form(
+            key: _form,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,17 +131,57 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                   "Delivery Details",
                   style: TextStyle(fontSize: 20),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "PickUpLocation"),
+                createTextFormField(
+                  contx: contx,
+                  label: "PickUpLocation",
+                  focus: _pickUpFocus,
+                  requestF: _reciverNameFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Reciver Name"),
+                createTextFormField(
+                  contx: contx,
+                  label: "Reciver Name",
+                  focus: _reciverNameFocus,
+                  requestF: _dropLocationFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Drop Location"),
+                createTextFormField(
+                  contx: contx,
+                  label: "Drop Location",
+                  focus: _dropLocationFocus,
+                  requestF: _deliveryPinCodeFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Delivery PinCode"),
+                createTextFormField(
+                  contx: contx,
+                  keyboard: TextInputType.number,
+                  label: "Delivery PinCode",
+                  focus: _deliveryPinCodeFocus,
+                  requestF: _lengthFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    if (value.length != 6) {
+                      return "6 digits pins";
+                    }
+                    return null;
+                  },
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 50),
@@ -61,8 +193,22 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                 Row(
                   children: [
                     Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: "L"),
+                      child: createTextFormField(
+                        contx: contx,
+                        keyboard: TextInputType.number,
+                        label: "L",
+                        focus: _lengthFocus,
+                        requestF: _breadthFocus,
+                        funValidate: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Field!";
+                          }
+                          if (double.tryParse(value) == null ||
+                              int.tryParse(value) == null) {
+                            return "0";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                     SizedBox(
@@ -73,8 +219,18 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                       width: 25,
                     ),
                     Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: "B"),
+                      child: createTextFormField(
+                        contx: contx,
+                        keyboard: TextInputType.number,
+                        label: "B",
+                        focus: _breadthFocus,
+                        requestF: _heightFocus,
+                        funValidate: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Field!";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                     SizedBox(
@@ -85,26 +241,74 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                       width: 25,
                     ),
                     Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: "H"),
+                      child: createTextFormField(
+                        contx: contx,
+                        label: "H",
+                        keyboard: TextInputType.number,
+                        focus: _heightFocus,
+                        requestF: _weightFocus,
+                        funValidate: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Field!";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ],
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Weight(kg)"),
+                createTextFormField(
+                  contx: contx,
+                  keyboard: TextInputType.number,
+                  label: "Weight(kg)",
+                  focus: _weightFocus,
+                  requestF: _productTitleFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Product Title"),
+                createTextFormField(
+                  contx: contx,
+                  label: "Product Title",
+                  focus: _productTitleFocus,
+                  requestF: _productCategoryFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "Product Category"),
+                createTextFormField(
+                  contx: contx,
+                  label: "Product Category",
+                  focus: _productCategoryFocus,
+                  requestF: _productPriceFocus,
+                  funValidate: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty Field!";
+                    }
+                    return null;
+                  },
                 ),
                 Row(
                   children: [
                     Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: "Prd.price"),
+                      child: createTextFormField(
+                        contx: contx,
+                        keyboard: TextInputType.number,
+                        label: "Prd.price",
+                        focus: _productPriceFocus,
+                        requestF: _quantityFocus,
+                        funValidate: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Field!";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                     SizedBox(
@@ -116,7 +320,20 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                     ),
                     Expanded(
                       child: TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        keyboardType: TextInputType.number,
+                        textInputAction: TextInputAction.done,
+                        focusNode: _quantityFocus,
                         decoration: InputDecoration(labelText: "Qyt"),
+                        onFieldSubmitted: (_) {
+                          opendDatePicker();
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Field!";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                     SizedBox(
@@ -141,9 +358,19 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                   children: [
                     Expanded(
                       child: ListTile(
-                        leading: Text("No delivery date selected "),
+                        leading: _isDateSelected
+                            ? Text(
+                                _selectedDate as String,
+                                style: TextStyle(
+                                  color: Theme.of(contx).primaryColor,
+                                  fontSize: 15,
+                                ),
+                              )
+                            : Text("No delivery date selected "),
                         trailing: TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            opendDatePicker();
+                          },
                           child: Text("Select Date"),
                         ),
                       ),
@@ -157,7 +384,9 @@ class CreateOrderScreenState extends State<CreateOrderScreen> {
                   width: double.infinity,
                   height: 50,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      onCreateOrder();
+                    },
                     child: Text("Create Order"),
                   ),
                 ),
